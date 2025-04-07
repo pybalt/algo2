37de820b1cb38aa4d135024c12a7b83cd60aa8da
Leonel Baltazar Bravo <96897286+pybalt@users.noreply.github.com>
2025-04-06 22:26:43 -0300

    Implement basic set operations in ConjuntoTA class: added methods to add, remove, choose an element, check membership, verify if the set is empty, and initialize the set.

--- programas/src/implementacion/ConjuntoTA.java
+++ programas/src/implementacion/ConjuntoTA.java
@@ -5,5 +5,37 @@ import tda.ConjuntoTDA;
 public class ConjuntoTA implements ConjuntoTDA {
     private int[] elementos;
     private int tamaño;
-    
+    public void agregar(int x){
+        if(!pertenece(x)){
+            elementos[tamaño] = x;
+            tamaño++;
+        }
+    }
+    public void sacar(int x){
+        int i = 0;
+        while(i < tamaño && elementos[i] != x){
+            i++;
+        }
+        if(i < tamaño){
+            elementos[i] = elementos[tamaño - 1];
+            tamaño--;
+        }
+    }
+    public int elegir(){
+        return elementos[tamaño - 1];
+    }
+    public boolean pertenece(int x){
+        int i = 0;
+        while(i < tamaño && elementos[i] != x){
+            i++;
+        }
+        return i < tamaño;
+    }
+    public int conjuntoVacio(){
+        return tamaño == 0;
+    }
+    public void inicializarConjunto(){
+        elementos = new int[100];
+        tamaño = 0;
+    }
 }
