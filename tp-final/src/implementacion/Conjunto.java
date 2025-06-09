4a71f0bf865e866a9c79bd67d7e8b7feebfb5855
Leonel Baltazar Bravo <96897286+pybalt@users.noreply.github.com>
2025-06-09 00:22:11 -0300

    Implement Conjunto class with methods for initialization, addition, removal, membership check, random selection, and emptiness check. Add TestConjunto class with comprehensive unit tests to validate functionality and edge cases.

--- tp-final/src/implementacion/Conjunto.java
+++ tp-final/src/implementacion/Conjunto.java
@@ -16,35 +16,70 @@ public class Conjunto implements ConjuntoTDA {
 	
 	@Override
 	public void inicializar() {
-
+		primero = null;
+		cantidad = 0;
+		r = new Random();
 	}
 
 	@Override
 	public void agregar(int valor) {
-
+		if(!pertenece(valor)){
+			nodo nuevoNodo = new nodo();
+			nuevoNodo.valor = valor;
+			nuevoNodo.siguiente = primero;
+			primero = nuevoNodo;
+			cantidad++;
+		}
 	}
 
 	@Override
 	public boolean pertenece(int valor) {
+		nodo actual = primero;
+		while(actual != null){
+			if(actual.valor == valor){
+				return true;
+			}
+			actual = actual.siguiente;
+		}
 		return false;
-
 	}
 
 	@Override
 	public void sacar(int valor) {
-
+		if(primero == null){
+			return;
+		}
+		// Caso 1. El primero es el valor
+		if(primero.valor == valor){
+			primero = primero.siguiente;
+			cantidad --;
+			return;
+		}
+		// Caso 2. El primero no es el valor.
+		nodo actual = primero;
+		while(actual.siguiente != null){
+			if(actual.siguiente.valor == valor){
+				actual.siguiente = actual.siguiente.siguiente;
+				cantidad--;
+				return;
+			}
+			actual = actual.siguiente;
+		}
 	}
 
 	@Override
 	public int elegir() {
-		return cantidad;
-
+		int qty = r.nextInt(cantidad);
+		nodo actual = primero;
+		for (int i = 0; i < qty; i++) {
+			actual = actual.siguiente;
+		}
+		return actual.valor;
 	}
 
 	@Override
 	public boolean estaVacio() {
-		return false;
-
+		return primero == null;
 	}
 
 }
